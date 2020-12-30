9e19c6a2e013b2e93f5b053cb2a83e8afa589cf4
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-12-29 18:08:55 -0700

    Updates

---
+++ precache-manifest.ae08d36633897281880fcde770d027b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c3b1220a09d42edca9961da84694d3",
+    "url": "/ultimatertis/index.html"
+  },
+  {
+    "revision": "9c65dc3683bf5e27c4ae",
+    "url": "/ultimatertis/static/css/main.50b3515d.chunk.css"
+  },
+  {
+    "revision": "eb4d3360ff8b2ebffb76",
+    "url": "/ultimatertis/static/js/2.6bca96ea.chunk.js"
+  },
+  {
+    "revision": "9c65dc3683bf5e27c4ae",
+    "url": "/ultimatertis/static/js/main.2b9b6ecc.chunk.js"
+  },
+  {
+    "revision": "01ecf323345a74da78f9",
+    "url": "/ultimatertis/static/js/runtime~main.bf91b1a6.js"
+  }
+]);
